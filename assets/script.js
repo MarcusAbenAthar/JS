230bcb98ee330576b3d16c766944a6a4101eedb1
Marcus Aben-Athar <marcus__aa@hotmail.com>
2024-07-03 11:07:54 -0300

    Calculadora de IMC OK

--- assets/script.js
+++ assets/script.js
@@ -7,7 +7,17 @@ function calculate() {
     alert("Insira apenas números!");
   } else {
     result = document.querySelector("#result").value = bmi.toFixed(2);
+    dataResult = document.querySelector("#data-result").style.display = "flex";
   }
-
   return result;
+}
+
+function reset() {
+  document.querySelector("#height").value = "";
+  document.querySelector("#weight").value = "";
+  document.querySelector("#name").value = "";
+  document.querySelector("#data-result").hidden = true;
+  document.querySelector("#data-result").style.display = "none";
+  alert("Campos limpos com sucesso!");
+  return (result = document.querySelector("#result").value = "");
 }
